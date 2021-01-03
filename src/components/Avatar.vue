9b6fe738d6b7ffde025c41e79af7f11b6cb035f6
michael mudge <michaeltmudge@gmail.com>
2021-01-03 07:39:52 -0800

    triangle behind avatar

--- src/components/Avatar.vue
+++ src/components/Avatar.vue
@@ -1,6 +1,9 @@
 <template>
-  <div v-if="style" class="elevation-5">
-    <div id="avatar" :style="style"></div>
+  <div style="position: relative;">
+    <div id="avatar-bg" class=""></div>
+    <div v-if="style" style="position: relative;" class="elevation-10">
+      <div id="avatar" :style="style"></div>
+    </div>
   </div>
 </template>
 
@@ -34,5 +37,20 @@ export default class Avatar extends Vue {
   background-size: cover;
   background-position: center;
   background-position: -50px;
+}
+
+#avatar-bg {
+  /* background-color: #efefef; */
+  width: 0px;
+  height: 0px;
+  border-top: 160px solid transparent;
+  border-right: 160px solid transparent;
+  border-left: 160px solid #e53935;
+  border-bottom: 160px solid #e53935;
+  border-radius: 2px;
+  position: absolute;
+  /* background-color: #e53935; */
+  bottom: -35px;
+  left: -35px;
 }
 </style>
