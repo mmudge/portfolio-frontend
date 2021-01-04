cf68ae3b672660c7d91a92d65633a6b80c5b0987
michael mudge <michaeltmudge@gmail.com>
2021-01-03 19:32:10 -0800

    added banner to project page

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,10 +1,24 @@
 <template>
   <div>
+    <PageBanner :fullScreen="false" :imageName="'laptop-2'">
+      <template v-slot:content>
+        <v-layout align-center justify-center>
+          <div class="text-center">
+            <h1 class="text-h2 font-weight-medium blackText1--text">
+              Projects
+            </h1>
+            <p class="text-h6 pt-5 font-weight-light mediumGray--text">
+              Some of my work over the past few years
+            </p>
+          </div>
+        </v-layout>
+      </template>
+    </PageBanner>
     <PageContainer>
       <template v-slot:content>
-        <h1 v-if="smAndDown" class="text-center pb-5 blackText1--text">
+        <!-- <h1 v-if="smAndDown" class="text-center pb-5 blackText1--text">
           Projects
-        </h1>
+        </h1> -->
         <template v-for="project in publishedProjects">
           <ProjectCard :key="project.id" :project="project" class="mb-5" />
         </template>
@@ -19,11 +33,13 @@ import Project from '@/models/Project'
 import AppComponent from '@/components/AppComponent'
 import PageContainer from '@/components/PageContainer.vue'
 import ProjectCard from '@/components/projects/ProjectCard.vue'
+import PageBanner from '@/components/PageBanner.vue'
 
 @Component({
   components: {
     PageContainer,
-    ProjectCard
+    ProjectCard,
+    PageBanner
   }
 })
 export default class Projects extends AppComponent {
